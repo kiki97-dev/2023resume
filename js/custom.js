53ddf16546146c90cf8a6b0947e90e54d347255b
lsc <dltmd1502@naver.com>
2023-04-11 17:44:58 +0900

    0.0.0v

---
+++ js/custom.js
@@ -0,0 +1,6 @@
+new fullpage('#fullpage', {
+    sectionsColor: ['yellow', 'orange', '#C0C0C0', '#ADD8E6'],
+
+    // Get your license at https://alvarotrigo.com/fullPage/pricing/
+    licenseKey: 'YOUR LICENSE KEY HERE '
+});
