61cb4113577ed8463e70318019c863493de5d76e
lsc <dltmd1502@naver.com>
2023-04-14 17:54:00 +0900

    0.1.6v 팝업추가

--- js/custom.js
+++ js/custom.js
@@ -5,8 +5,11 @@ $(function () {
 
     let $portfolioList = $(".portfolio-list>article");
     let $popup = $(".popup");
+    let $popupArticle = $popup.find("article");
+
     let $body = $("body");
 
+    var player;
 
     $(window).load(function () {
 
@@ -62,16 +65,42 @@ $(function () {
 
     $portfolioList.on("click", function () {
         $popup.addClass("on");
+        $popupArticle.eq($(this).index()).addClass("on");
         $body.addClass("no-scroll ");
     })
 
     $(".popup__close").on("click", function (e) {
         e.preventDefault();
         $popup.removeClass("on");
+        $popupArticle.removeClass("on");
         $body.removeClass("no-scroll ");
     })
 
 })
+
+
+/* function onYouTubeIframeAPIReady() {
+    player = new YT.Player('player', {
+        videoId: 'CFhc3Vg7V6c', //유튭 영상ID
+        playerVars: {
+            'autoplay': 1, //자동재생
+            'rel': 0,
+            'controls': 1,	//youtube 콘트롤 표시
+            'loop': 1, //반복 여부  
+            'playlist': 'CFhc3Vg7V6c',
+        },
+        events: {
+            'onReady': onPlayerReady,
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    player.playVideo();
+    player.mute(); //자동재생하려면 mute해야함
+} */
+
+
 
 function copyToClipboard(val) {
     var t = document.createElement("textarea");
